7119a3e63b8c13920fbadf1539367f428602f726
Jonathan Choy <jonochoyo@gmail.com>
2023-08-10 09:46:29 +0930

    Edit headers

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jchoy-me <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 12:18:46 by jchoy-me          #+#    #+#             */
-/*   Updated: 2023/08/09 17:29:23 by jchoy-me         ###   ########.fr       */
+/*   Updated: 2023/08/10 09:45:17 by jchoy-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <stdarg.h>
 # include <stdlib.h>
 
+int		ft_printf(const char *format, ...);
+
 int		ft_print_c(char c);
 int		ft_print_s(char *s);
 int		ft_print_di(int n);
@@ -24,6 +26,5 @@ int		ft_print_u(unsigned int n);
 int		ft_print_x(unsigned int n, char c);
 int		ft_print_p(size_t ptr);
 int		ft_print_percent(void);
-int		ft_printf(const char *format, ...);
 
 #endif
